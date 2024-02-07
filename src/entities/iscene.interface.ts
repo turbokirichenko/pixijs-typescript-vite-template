1dc6e37a0eed98e1ae41b5fb24eb630651365b96
turbokirichenko <turbokirichenko@outlook.com>
2024-02-07 18:29:41 +0200

    remove old file

--- src/entities/iscene.interface.ts
+++
@@ -1,5 +0,0 @@
-export interface IScene {
-    update(framesPassed: number): void;
-    // we added the resize method to the interface
-    resize(screenWidth: number, screenHeight: number): void;
-}
